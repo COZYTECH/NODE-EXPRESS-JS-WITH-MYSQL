d82d8c536975cef472338caae0984d7572ef0b28
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2025-11-04 11:25:54 +0100

    more update an security check

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,10 +1,32 @@
-import users from "../model/user.model.js";
+import Users from "../model/user.model.js";
+
 export const registerUser = async (req, res) => {
   const { email, password } = req.body;
   try {
-    let newUser = new users(email, password);
-    newUser = await newUser.save();
-    res.send({ message: "User registered!", newUser });
+    //input validation
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Email and password are required" });
+    }
+    //validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(400).send({ message: "Invalid email format" });
+    }
+    if (password.length < 8) {
+      return res
+        .status(400)
+        .send({ message: "Password must be at least 8 characters" });
+    }
+
+    const userInstance = new Users(email, password);
+    await userInstance.save();
+
+    res.send({
+      message: "User registered!",
+      user: { email: userInstance.email },
+    });
   } catch (error) {
     console.log("Error registering user:", error);
     res.status(500).send({ message: "Internal Server Error" });
--- model/user.model.js
+++ model/user.model.js
@@ -1,18 +1,21 @@
 import db from "../config/db.js";
+import bcrypt from "bcrypt";
 
-class users {
+class Users {
   constructor(email, password) {
     this.email = email;
     this.password = password;
   }
   async save() {
-    let sql = `
-        INSERT INTO users (email, password) VALUES (
-        '${this.email}', '${this.password}'
-        )
-        `;
-    const [result, _] = await db.execute(sql);
+    // let sql = `
+    //     INSERT INTO users (email, password) VALUES (
+    //     '${this.email}', '${this.password}'
+    //     )
+    //     `;
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    let sql = `INSERT INTO users (email, password) VALUES (?, ?)`;
+    const [result, _] = await db.execute(sql, [this.email, hashedPassword]);
     return result;
   }
 }
-export default users;
+export default Users;
